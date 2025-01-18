53e04259fae40f3aff824bfae739120742974af6
ianicakomputek <46000884+ianicakomputek@users.noreply.github.com>
2025-01-18 15:59:49 +0800

    Add files via upload

--- cakbas.sh
+++ cakbas.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /etc/openvpn/
 rm *.conf
-wget https://github.com/ianicakomputek/r8mart/blob/master/iilaptop.conf
+wget https://raw.githubusercontent.com/ianicakomputek/r8mart/refs/heads/main/iilaptop.conf
 /etc/rc.d/rc.openvpn restart
