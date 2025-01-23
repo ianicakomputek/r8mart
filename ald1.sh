5f825438956455b6a7527523c6a090b24ed6d0a7
ianicakomputek <46000884+ianicakomputek@users.noreply.github.com>
2025-01-23 14:15:38 +0800

    Add files via upload

---
+++ ald1.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd /etc/openvpn/
+rm *.conf
+wget --no-check-certificate https://raw.githubusercontent.com/ianicakomputek/r8mart/refs/heads/main/aldkas2_1.conf
+/etc/rc.d/rc.openvpn restart
